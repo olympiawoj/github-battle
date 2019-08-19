55f47ae9265f58664d2e52c84c96554580971e40
olympia <owojcik7@gmail.com>
2019-08-18 22:18:59 -0400

    Add player input form

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
+import PropTypes from "prop-types";
 
 function Instructions() {
   return (
@@ -26,11 +27,73 @@ function Instructions() {
     </div>
   );
 }
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.username);
+  }
+
+  handleChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  render() {
+    return (
+      <>
+        <form className="column player" onSubmit={this.handleSubmit}>
+          <label htmlFor="username" className="player-label">
+            {this.props.label}
+          </label>
+          <div className="row player-inputs">
+            {/* controlled component - the value & state of input field is going to live inside of the React component on local state& that means to update input field, must update state*/}
+            <input
+              type="text"
+              id="username"
+              className="input-light"
+              placeholder="github-username"
+              autoComplete="off"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+
+            <button
+              className="btn dark-btn"
+              type="submit"
+              disabled={!this.state.username}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </>
+    );
+  }
+}
+
+//Adding static property propTypes on component input
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+};
+
 export default class Battle extends React.Component {
   render() {
     return (
       <>
         <Instructions />
+        <PlayerInput label="Label" onSubmit={value => console.log(value)} />
       </>
     );
   }

e56ea126dc7a593e85fa75ad9774cb36b44c4b26
olympia <owojcik7@gmail.com>
2019-08-19 20:51:11 -0400

    Render PlayerInput and add handleSubmit

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -89,11 +89,42 @@ PlayerInput.propTypes = {
 };
 
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, player) {
+    this.setState({ [id]: player });
+  }
+
   render() {
+    const { playerOne, playerTwo } = this.state;
     return (
       <>
         <Instructions />
-        <PlayerInput label="Label" onSubmit={value => console.log(value)} />
+        <div className="players-container">
+          <h1 className="center-text header-lg"> Players</h1>
+          <div className="row space-around">
+            {playerOne === null && (
+              <PlayerInput
+                label="player-one"
+                onSubmit={player => this.handleSubmit("playerOne", player)}
+              />
+            )}
+            {playerTwo === null && (
+              <PlayerInput
+                label="player-two"
+                onSubmit={player => this.handleSubmit("playerTwo", player)}
+              />
+            )}
+          </div>
+        </div>
       </>
     );
   }

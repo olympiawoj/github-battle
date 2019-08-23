baca549003ea0bc60835ea00bb61eb1345c306bf
olympia <owojcik7@gmail.com>
2019-08-22 22:33:38 -0400

    Update API to get information about users after battle

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { battle } from "../utils/api";
 
+//Inside results, we want to fetch info about our two users
 export default class Results extends React.Component {
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props;
+    battle([playerOne, playerTwo]).then(players => {
+      console.log("data: ", players);
+    });
+  }
   render() {
     return (
       <div>
